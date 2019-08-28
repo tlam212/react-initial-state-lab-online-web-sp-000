605902535dd62ec2a25ff9b10b84cfdbbd59d4e2
Toni L <tlam212@gmail.com>
2019-08-28 20:13:09 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -10,8 +10,10 @@ export default class Bomb extends React.Component{
   }
   
   render(){
+    
+    let message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
     return(
-      <p>{this.state.secondsLeft} seconds left before I go boom!</p>)
+      <p>{message}</p>
   }
 
 }
