bf5a5f22d7cd94367d5055fe363c84db7c886ed4
Toni L <tlam212@gmail.com>
2019-08-28 20:13:49 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -12,8 +12,9 @@ export default class Bomb extends React.Component{
   render(){
     
     let message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
-    return(
-      <p>{message}</p>
+    
+    
+    return(<p>{message}</p>
       )
   }
 
