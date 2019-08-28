0a72c43788b30f3f3fd181f765658890088bf49b
Toni L <tlam212@gmail.com>
2019-08-28 20:04:56 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -9,7 +9,7 @@ export default class ImageSlider extends React.Component{
     }
   }
   
-  increment = () => {
+  increment() = {
     this.setState = ({
       currentSlideIndex = this.state.currentSlideIndex + 1
     })
